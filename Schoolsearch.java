df1272060c6045c67aa87b11ae80c205bd1f5c00
Amanda <mndcheng@github.com>
2018-01-10 21:45:20 -0800

    added if statements in the main and added to student function

--- Schoolsearch.java
+++ Schoolsearch.java
@@ -44,60 +44,86 @@ public class Schoolsearch {
       public String getTFirstName() { return tFirstName; }
     
   }
-// test
+
   public static void main(String[] args) {
 
-        ArrayList<Student> students = readFile();
+        ArrayList<Student> students = new ArrayList<Student>();
         Scanner sc = new Scanner(System.in);
         String in = "";
-        String searchVal; 
+        String[] inList; 
+
+        readFile(students); 
 
         System.out.print("What do you want to search for? ");
 
-        while (!in.equals("Q") && !in.equals("Quit")) { // how to check for Q or Quit
+        while (!in.equals("Q") && !in.equals("Quit")) {
             in = sc.nextLine();
-            if (in.contains("S")) {
-                // searchFile for <lastname>
-            }
+            inList = in.split(" "); 
+
+            String instruction = inList[0]; 
+            String lastNameNum = inList[1]; 
+            if (instruction.equals("S:") || instruction.equals("Student:")) {
+                int listSize = inList.length; 
+                if (listSize == 2) {
+                    student(students, lastNameNum, null); 
+                } else if (listSize == 3) {
+                    student(students, lastNameNum, inList[2]); 
+                }
+            } else if (instruction.equals("T:") || instruction.equals("Teacher:")) {
+                //teacher(students, lastNameNum); 
+            } else if (instruction.equals("G:") || instruction.equals("Grade:")) {
+                //grade(students, lastNameNum); 
+            } else if (instruction.equals("B:") || instruction.equals("Bus:")) {
+                //bus(students, lastNameNum); 
+            } // incomplete 
             System.out.print("What do you want to search for? ");
         }
 
   }
 
-  private void Student(ArrayList<Student> students, String lastName, String bus) {
+  private void student(ArrayList<Student> students, String lastName, String bus) {
+      int listSize = students.size(); 
+
+      for (int i = 0; i < listSize; i++) {
+          if (students.get(i).contains(lastName)) {
+              if (bus == null) {
+                System.out.print(students.get(i).getStLastName() + " " + students.get(i).getStFirstName() + " "); 
+                System.out.print(students.get(i).getGrade() + " " + students.get(i).getClassroom() + " "); 
+                System.out.println(students.get(i).getTLastName() + " " + students.get(i).getTFirstName()); 
+              } else {
+                System.out.println(students.get(i).getStLastName() + " " + students.get(i).getStFirstName() + " " + students.get(i).getBus()); 
+              }
+          }
+      }
+  }
+
+  private void teacher(ArrayList<Student> students, String lastName) {
 
   }
 
-  private void Teacher(ArrayList<Student> students, String lastName) {
+  private void grade(ArrayList<Student> students, String number) {
 
   }
 
-  private void Grade(ArrayList<Student> students, String number) {
+  private void bus(ArrayList<Student> students, String number) {
 
   }
 
-  private void Bus(ArrayList<Student> students, String number) {
+  private void gradeHL(ArrayList<Student> students, int HL) {
 
   }
 
-  private void GradeHL(ArrayList<Student> students, int HL) {
+  private void average(ArrayList<Student> students, String number) {
 
   }
 
-  private void Average(ArrayList<Student> students, String number) {
+  private void info(ArrayList<Student> students) {
 
   }
 
-  private void Info(ArrayList<Student> students) {
+  private void readFile(ArrayList<Student> list) {
 
-  }
-
-
-  private ArrayList<Student> readFile() {
-
-      ArrayList<Student> students = new ArrayList<Student>();
       String fileName = "students.txt";
-
       String line = null;
 
       try {
@@ -110,14 +136,13 @@ public class Schoolsearch {
               String[] tokens = line.split(",");
               Student s = new Student(tokens[0], tokens[1], tokens[2], tokens[3],
                       tokens[4], tokens[5], tokens[6], tokens[7]);
-              students.add(s);
+              list.add(s);
 
           }
 
       } catch(IOException e) {
           System.out.println("File not found.");
       }
-      return students;
   }
 
 }
